7b61e4fe01d388625781f92c0459f7e4eab410ff
somunachima <somunachima@gmail.com>
2023-09-28 15:44:30 +0100

    Add task formatter file for final challenge for unit testing peer classes

---
+++ lib/task_formatter.py
@@ -0,0 +1,11 @@
+class TaskFormatter:
+    def __init__(self, task): # task is an instance of Task
+        pass
+
+    def format(self):
+        # Returns the task formatted as a string.
+        # If the task is not complete, the format is:
+        # - [ ] Task title
+        # If the task is complete, the format is:
+        # - [x] Task title
+        pass
